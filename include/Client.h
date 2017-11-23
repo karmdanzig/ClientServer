b416f6936c5edbf4e9b91fbd69ba20f86e2c2293
Carmelo Porcelli <karm.danzig@gmail.com>
2017-11-23 10:52:48 +0000

    Update Client.h

--- include/Client.h
+++ include/Client.h
@@ -8,16 +8,16 @@
 class Client
 {
 public:
-	Client(const std::string& IP, const int port);
-	virtual ~Client();
-	void init();
-	void sendRequestToServer(const std::string& startingCity, const std::string& destinationCity);
+    Client(const std::string& IP, const int port);
+    virtual ~Client();
+    void init();
+    void sendRequestToServer(const std::string& startingCity, const std::string& destinationCity);
 
 private:
-	std::string m_IP;
-	int m_port;
-	int m_clientSocket;
-	sockaddr_in m_socketStruct;
+    std::string m_IP;
+    int m_port;
+    int m_clientSocket;
+    sockaddr_in m_socketStruct;
 };
 
 #endif // CLIENT_H
