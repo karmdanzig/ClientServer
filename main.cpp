c1afefe613fa9fc1488c052b0b13e6740dafd09e
Carmelo Porcelli <karm.danzig@gmail.com>
2017-11-23 14:24:07 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
-#include <string.h>
 #include "Server.h"
 #include "Client.h"
 #include "ClientAdmin.h"
 
 int main(int argc, char* argv[])
 {
-
-    if (strcmp(argv[1],"Client") == 0)
+    if(argc < 2)
     {
+        std::cout << "You must provide at least one argument between Server, Client, and ClientAdmin" << std::endl;
+        return -1;
+    }
+    
+    std::string option1(argv[1]);
+    
+    if (option1 == "Client")
+    {
+        if (argc < 4)
+        {
+            std::cout << "You must provide at least two cities to perform a successful request" << std::endl;
+            return -1;
+        }
         Client c("127.0.0.1", 5555);
         c.init();
         c.sendRequestToServer(argv[2], argv[3]);
@@ -28,7 +39,8 @@ int main(int argc, char* argv[])
     }
     else
     {
-        std::cout << "Wrong usage" << std::endl;
+        std::cout << "You must provide at least one argument between Server, Client, and ClientAdmin" << std::endl;
+        return -1;
     }
 
     return 0;
