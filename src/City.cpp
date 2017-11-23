9e107f7730c37638893d32ef3db593cc36f936d8
Carmelo Porcelli <karm.danzig@gmail.com>
2017-11-23 12:20:06 +0000

    Update City.cpp

--- src/City.cpp
+++ src/City.cpp
@@ -1,7 +1,7 @@
 #include "City.h"
 #include <algorithm>
 
-City::City(const std::string& name, const int points) : name(name), points(points)
+City::City(const std::string& name, const int points) : m_name(name), m_points(points)
 {
 }
 
@@ -15,29 +15,29 @@ void City::addNeighbor(City* toAdd)
     {
         std::cout << "cannot add self as neighbor" << std::endl;
     }
-    else if (std::find(neighbors.begin(), neighbors.end(),toAdd) != neighbors.end())
+    else if (std::find(m_neighbors.begin(), m_neighbors.end(), toAdd) != m_neighbors.end())
     {
         std::cout << "already present" << std::endl;
     }
     else
     {
-        neighbors.push_back(toAdd);
+        m_neighbors.push_back(toAdd);
     }
 }
 
 const int City::getPoints() const
 {
-    return this->points;
+    return this->m_points;
 }
 
 const std::vector<City*>& City::getNeighbors() const
 {
-    return this->neighbors;
+    return this->m_neighbors;
 }
 
 const std::string& City::getName() const
 {
-    return this->name;
+    return this->m_name;
 }
 
 bool operator<(const City& a, const City& b)
@@ -49,7 +49,7 @@ std::ostream& operator<<(std::ostream& out, const City& toPrint)
 {
     out << toPrint.getName();
     out << " with neighbors: ";
-    for(std::vector<City*>::const_iterator it = toPrint.neighbors.begin(); it != toPrint.neighbors.end(); ++it)
+    for(std::vector<City*>::const_iterator it = toPrint.m_neighbors.begin(); it != toPrint.m_neighbors.end(); ++it)
     {
         out << (*it)->getName() << " ";
     }
