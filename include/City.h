678017ce970796fc1e70370f7781e1bf83ebd72c
Carmelo Porcelli <karm.danzig@gmail.com>
2017-11-23 10:53:27 +0000

    Update City.h

--- include/City.h
+++ include/City.h
@@ -8,19 +8,19 @@
 class City
 {
 public:
-	City(const std::string& name, const int points);
-	virtual ~City();
-	void addNeighbor(City* toAdd);
-	const int getPoints() const;
-	const std::vector<City*>& getNeighbors() const;
-	const std::string& getName() const;
-	friend bool operator<(const City& a, const City& b);
-	friend std::ostream& operator<<(std::ostream& out, const City& toPrint);
+    City(const std::string& name, const int points);
+    virtual ~City();
+    void addNeighbor(City* toAdd);
+    const int getPoints() const;
+    const std::vector<City*>& getNeighbors() const;
+    const std::string& getName() const;
+    friend bool operator<(const City& a, const City& b);
+    friend std::ostream& operator<<(std::ostream& out, const City& toPrint);
 
 private:
-	int points;
-	std::string name;
-	std::vector<City*> neighbors;
+    int points;
+    std::string name;
+    std::vector<City*> neighbors;
 };
 
 #endif // CITY_H
