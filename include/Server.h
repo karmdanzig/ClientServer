adcfe6b57e66ac6e77b38698cddcc82227f5fc6d
Carmelo Porcelli <karm.danzig@gmail.com>
2017-11-23 10:54:00 +0000

    Update Server.h

--- include/Server.h
+++ include/Server.h
@@ -10,28 +10,28 @@
 class Server
 {
 public:
-	Server(const std::string& IP, const int port);
-	virtual ~Server();
-	void init();
-	void loadCities();
-	void receiveFromClient();
+    Server(const std::string& IP, const int port);
+    virtual ~Server();
+    void init();
+    void loadCities();
+    void receiveFromClient();
 
 private:
-	std::string m_IP;
-	int m_port;
-	int m_serverSocket;
-	int m_clientSocket;
-	sockaddr_in m_serverSocketStruct;
-	sockaddr_in m_clientSocketStruct;
-	std::vector<City> m_cities;
+    std::string m_IP;
+    int m_port;
+    int m_serverSocket;
+    int m_clientSocket;
+    sockaddr_in m_serverSocketStruct;
+    sockaddr_in m_clientSocketStruct;
+    std::vector<City> m_cities;
 
-	void printCities();
-	City* returnCityByName(const std::string& nameOfCityToRetrieve);
-	void getMinimumDistanceAndSendBackToClient(const City *departureCity, const City *destinationCity);
-	int shortestPath(const City *departureCity, const City *destinationCity);
-	void sendResultToClient(const std::string& toSend);
-	std::string addAnotherCity(const std::string& toParse);
-	void addCityToCurrentListAndSendBackToClient(const std::string& toParse);
+    void printCities();
+    City* returnCityByName(const std::string& nameOfCityToRetrieve);
+    void getMinimumDistanceAndSendBackToClient(const City *departureCity, const City *destinationCity);
+    int shortestPath(const City *departureCity, const City *destinationCity);
+    void sendResultToClient(const std::string& toSend);
+    std::string addAnotherCity(const std::string& toParse);
+    void addCityToCurrentListAndSendBackToClient(const std::string& toParse);
 };
 
 #endif // SERVER_H
